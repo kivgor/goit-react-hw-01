08ef4141b28db934bf3861152e0aa92e0703e414
Igor <pa220282kiv@gmail.com>
2024-12-09 22:03:28 +0200

    Test changes

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import Product from './Product/Product';
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      {
+        <div>
+          <h1>Best selling</h1>
+          <Product />
+          <Product />
+          <Product />
+          
+        </div>
+      }
+    </>
+  )
+}
+
+export default App
